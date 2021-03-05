5f99a1ec4d399d829292cf7d6caaa86c2514a73d
amandahp <amandahoffmann96@gmail.com>
2021-03-05 16:49:40 -0300

    feat: add button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import PdfViewer from "./PdfViewer";
 import PDF from "./teste.pdf";
 import "./App.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 const App = () => (
-  <Document
-    file={PDF}
-    onContextMenu={(e) => e.preventDefault()}
-    className="pdf-container"
-  >
-    <Page pageNumber={1} />
-  </Document>
+  <div className="content">
+    <h1>Example View PDF</h1>
+    <PdfViewer pdf={PDF} />
+  </div>
 );
 
 export default App;
